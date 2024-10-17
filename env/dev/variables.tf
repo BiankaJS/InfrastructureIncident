3bdb018d15944ad224f8d8b6f9881ce1f0e82387
Bianka <biankasarayj@outlook.com>
2024-10-17 00:01:24 -0600

    Initialized project

---
+++ env/dev/variables.tf
@@ -0,0 +1,35 @@
+variable "MAIL_SECRET_KEY" {
+  type = string
+}
+
+variable "MAPBOX_ACCESS_TOKEN" {
+  type = string
+}
+
+variable "MAIL_SERVICE" {
+  type = string
+}
+
+variable "MAIL_USER" {
+  type = string
+}
+
+variable "MONGO_INITDB_ROOT_PASSWORD" {
+  type = string
+}
+
+variable "MONGO_INITDB_ROOT_USERNAME" {
+  type = string
+}
+
+variable "DOMAIN" {
+  type = string
+}
+
+variable "PORT" {
+  type = string
+}
+
+variable "MONGO_URL" {
+  type = string
+}
